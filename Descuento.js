903644b3d6e411036cb3a4686df3f52ad9cb9033
Esteban Guerrero <esteban.13guerrero@gmail.com>
2015-09-28 13:45:48 -0600

    final

--- Descuento.js
+++ Descuento.js
@@ -1,25 +1,27 @@
 var Converter = require("csvtojson").Converter;
+var json2csv = require("json2csv");
+var fs = require('fs');
 var converter = new Converter({});
-
- 
+var fields = ['ID','Negociacion'];
 converter.on("end_parsed", function (jsonArray) {
    var k = jsonArray;
 for (var key in jsonArray) {
   if (jsonArray.hasOwnProperty(key)) {
      var id= JSON.stringify(jsonArray[key].ID);
-     console.log(" ");
-     console.log(id);
-     console.log("");
-     console.log(key + " -> " + JSON.stringify(jsonArray[key].Negociacion));
-     var Negociaciones = new Array();
-     Negociaciones = JSON.stringify(jsonArray[key].Negociaciones[1].Familia);
-     console.log(" ");
-     console.log("->"+Negociaciones);
-     console.log(" ");
-  
+     var negociaciones=jsonArray[key].Negociacion;
+     for (var key in negociaciones){
+     if(negociaciones[key].Familia == 120 && (negociaciones[key].SubFamilia == 66)){
+     negociaciones[key].Descuento = 12 ; 
+     }
+     }
   }
 }
+ json2csv({ data: jsonArray, fields: fields }, function(err, csv) {
+   if (err) console.log(err);
+       fs.writeFile('cambioDescuento.csv', csv, function(err) {
+   if (err) throw err;
+    console.log('Se ha cambiado los valores, archivo guardado como cambioDescuento.csv');
+  });
 });
-
-
+});
 require("fs").createReadStream("./descuento.csv").pipe(converter);
