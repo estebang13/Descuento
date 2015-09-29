f30b5fb21dca75f760e485d779b768a713e62259
Esteban Guerrero <esteban.13guerrero@gmail.com>
2015-09-29 14:54:51 -0600

    Final

--- Descuento.js
+++ Descuento.js
@@ -3,24 +3,32 @@ var json2csv = require("json2csv");
 var fs = require('fs');
 var converter = new Converter({});
 var fields = ['Record ID','Negociacion'];
+
+
 converter.on("end_parsed", function (jsonArray) {
-   var k = jsonArray;
-for (var key in jsonArray) {
+for (var key in jsonArray) { //Se recorren las cuentas para obtener los datos de cada una
   if (jsonArray.hasOwnProperty(key)) {
-     var negociaciones=jsonArray[key].Negociacion;
-     for (var key in negociaciones){
-     if(negociaciones[key].Familia == 120 && (negociaciones[key].SubFamilia == 66)){
-     negociaciones[key].Descuento = 12 ; 
+     var negociaciones=jsonArray[key].Negociacion; // Se obtiene los Datos de Negociacion
+     for (var key1 in negociaciones){ //Se recorren las negociaciones y se buscan las considencias para cambiarlo
+     if(negociaciones[key1].Familia == 120 && (negociaciones[key1].SubFamilia == 66)){
+     negociaciones[key1].Descuento = 12 ; 
      }
      }
+     var aux = JSON.stringify(negociaciones);
+    jsonArray[key].Negociacion = aux; // Esto es para darle el formato adecuado(No me lo generaba bien)
   }
 }
- json2csv({ data: jsonArray, fields: fields }, function(err, csv) {
+
+
+ json2csv({ data: jsonArray, fields: fields }, function(err, csv) { // Conversion de los datos a CSV.
    if (err) console.log(err);
        fs.writeFile('cambioDescuento.csv', csv, function(err) {
    if (err) throw err;
     console.log('Se ha cambiado los valores, archivo guardado como cambioDescuento.csv');
   });
 });
+
 });
+
+
 require("fs").createReadStream("./descuento.csv").pipe(converter);
